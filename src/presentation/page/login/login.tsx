5ae4722a63b2710d39639ca0877fb94621aee9fd
rodrigojustinodevs <rodrigojustino.devs@gmail.com>
2025-02-11 17:08:43 -0300

    feat: change login page and add logo image

--- src/presentation/page/login/login.tsx
+++ src/presentation/page/login/login.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@presentation/components/button";
+import { Input } from "@presentation/components/form/input";
 import { memo } from "react";
+import logo from "@assets/EXPERMED.webp"
 
 const Login = memo(() => {
-    return <>login</>
+    return <div className="flex w-100 h-full items-center justify-center">
+        <div className="w-full flex flex-col gap-6 p-4 bg-white rounded-lg shadow-xl">
+            <div>
+                <picture className="w-100 h-12">
+                    <img src={logo} alt="Login logo" />
+                </picture>
+            </div>
+            <div className="w-full flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
+                    <label className="" htmlFor="email">Email</label>
+                    <Input className="w-100 border-white" id="email" placeholder="Enter your email" />
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label className="" htmlFor="password">password</label>
+                    <Input className="w-100 border-white" id="password" placeholder="Enter your password" />
+                </div>
+            </div>
+            <Button className="text-white">
+                Login
+            </Button>
+        </div>
+    </div>
 })
 
-export { Login }
+export { Login };
