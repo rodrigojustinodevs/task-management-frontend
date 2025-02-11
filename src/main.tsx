0ded3eb7bccc4d90ea64317ecc7d1b12e5ad59c7
rodrigojustinodevs <rodrigojustino.devs@gmail.com>
2025-02-11 16:46:45 -0300

    feat: configure tailwind css and apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import client from '@infra/apolloClient/apolloClient';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   darkMode: ["class", ""],
   important: true,
-content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-  extend: {
-    colors: {
-      primary: "#0f2a59"
+    extend: {
+      colors: {
+        primary: "#0f2a59"
+      }
     }
-  }
-},
-plugins: []
+  },
+  plugins: []
 };
